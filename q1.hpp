a603f75d69cfbc3a7cdcc7eb7ccc0fd56c89d32c
LEEEJMONA <123528104+LEEEJMONA@users.noreply.github.com>
2023-03-19 16:05:00 -0700

    6-1

--- q1.hpp
+++ q1.hpp
@@ -2,10 +2,35 @@
 #include <iomanip>
 using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+void swapTwoValues(int& ,int&);
+void swapTwoValues(float&,float&);
+void getinput (int &, int &);
+void getinput (float &, float &);
+int n1, n2;
+float f1, f2;
 
-// ******************************
-// Implement all your functions here
-// ******************************
+
+void getinput(int &n1, int &n2)
+{
+    cin >> n1 >> n2;
+}
+
+void getinput(float &f1, float &f2)
+{
+    cin >> f1 >> f2;
+}
+
+void swapTwoValues(int& n1, int& n2)
+{
+    int temp = n1;
+    n1 = n2;
+    n2 = temp;
+}
+
+
+void swapTwoValues(float& f1, float& f2)
+{
+    float temp = f1;
+    f1 = f2;
+    f2 = temp;
+}
